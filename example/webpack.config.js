9a9ee541a6eeda37a8232675a233c7b23839a90b
Seven <shiweidu@outlook.com>
2017-04-07 23:50:40 +0800

    增加更多使用描述。

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -16,8 +16,16 @@ var WebpackLaravelMixManifestObject = require('../dist/main.js');
 var transform = WebpackLaravelMixManifestObject.transform;
 var WebpackLaravelMixManifest = WebpackLaravelMixManifestObject.default;
 
+// Transform is optional.
+// 
 // ES6 code:
 // import WebpackLaravelMixManifest, { transform } from '../dist/main.js';
+// 
+// package use:
+// import WebpackLaravelMixManifest, { transform } from 'webpack-laravel-mix-manifest';
+// 
+// If you want to use es5's original code to produce the laravel-mix function compatible plugin:
+// import WebpackLaravelMixManifest, { transform } from 'webpack-laravel-mix-manifest/src/main.js'; 
 
 module.exports = {
   cache: true,
